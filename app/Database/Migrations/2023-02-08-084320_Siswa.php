3ab050c42e5b0ceb602f38e8a2520bba0e8931c7
dwipangestu <dwiaje19@gmail.com>
2023-02-08 15:51:01 +0700

    menambahkan migrate siswa

---
+++ app/Database/Migrations/2023-02-08-084320_Siswa.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Siswa extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'auto_increment' => true,
+            ],
+            'nama' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '100',
+            ],
+            'alamat' => [
+                'type' => 'TEXT',
+            ],
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('siswa');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('siswa');
+    }
+}
